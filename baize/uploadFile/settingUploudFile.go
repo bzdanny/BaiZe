a1fcd7f6f8bd813afa4c9f76bbcf92fedd193cb4
Danny <zhao_402295440@126.com>
2022-08-09 23:01:51 +0800

    升级2.0.0

---
+++ baize/uploadFile/settingUploudFile.go
@@ -0,0 +1,25 @@
+package uploadFile
+
+type UploadFile struct {
+	Type       string `mapstructure:"type"`
+	DomainName string `mapstructure:"domain_name"`
+	*S3        `mapstructure:"s3"`
+	*Eos       `mapstructure:"eos"`
+	*Localhost `mapstructure:"localhost"`
+}
+type S3 struct {
+	accessKeyId     string `mapstructure:"access_key_id"`
+	SecretAccessKey string `mapstructure:"Secret_access_key"`
+	Region          string `mapstructure:"region"`
+	BucketName      string `mapstructure:"bucket_name"`
+}
+type Eos struct {
+	accessKeyId     string `mapstructure:"access_key_id"`
+	SecretAccessKey string `mapstructure:"Secret_access_key"`
+	Url             string `mapstructure:"url"`
+	BucketName      string `mapstructure:"bucket_name"`
+}
+type Localhost struct {
+	PublicResourcePrefix  string `mapstructure:"public_resource_prefix"`
+	PrivateResourcePrefix string `mapstructure:"private_resource_prefix"`
+}
